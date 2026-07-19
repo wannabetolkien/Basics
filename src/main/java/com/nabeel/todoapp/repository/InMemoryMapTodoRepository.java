2781a93b67d4cf8fdd63b642e5951e6f62f13c3b
wannabetolkien <akhternabeel1411@gmail.com>
2026-07-19 20:06:11 +0530

    Minor Changes

---
+++ src/main/java/com/nabeel/todoapp/repository/InMemoryMapTodoRepository.java
@@ -0,0 +1,19 @@
+package com.nabeel.todoapp.repository;
+import com.nabeel.todoapp.schema.Todo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class InMemoryMapTodoRepository implements ITodoRepository{
+
+    private Map<String,Todo> todos = new HashMap<>();
+
+    public List<Todo> findAll(){
+        return new ArrayList<Todo>(todos.values());
+    }
+}
--- src/main/java/com/nabeel/todoapp/services/todoService.java
+++ src/main/java/com/nabeel/todoapp/services/todoService.java
@@ -3,17 +3,16 @@ package com.nabeel.todoapp.services;
 import org.springframework.stereotype.Service;
 import com.nabeel.todoapp.schema.Todo;
 
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 
 import com.nabeel.todoapp.repository.ITodoRepository;
 
 @Service
+@AllArgsConstructor
 public class todoService {
     private ITodoRepository todoRepository;
-
-    public todoService(ITodoRepository todoRepository){
-        this.todoRepository = todoRepository;
-    }
 
     public List<Todo> getAllTodos(){
         return todoRepository.findAll();
